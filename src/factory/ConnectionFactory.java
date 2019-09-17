8ca6d4e7ee57d256792f45bc6136a33a3048a07f
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2019-09-17 00:18:41 -0300

    pacote factory com problemas
    pacote factory com problemas

--- src/factory/ConnectionFactory.java
+++ src/factory/ConnectionFactory.java
@@ -1,4 +1,4 @@
-package br.com.projeto.factory;
+package factory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
--- src/factory/TestaConexao.java
+++ src/factory/TestaConexao.java
@@ -1,14 +1,13 @@
-package br.com.projeto.factory;
+package factory;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class TestaConexao {
     
     public static void main(String[] args) throws SQLException{
-        Connection connection;
-        connection = new ConnectionFactory().getConnection();
-        System.out.println("Conexão aberta!");
-        connection.close();
+        try (Connection connection = new ConnectionFactory().getConnection()) {
+            System.out.println("Conexão aberta!");
+        }
     }//void main
     
 }//class
